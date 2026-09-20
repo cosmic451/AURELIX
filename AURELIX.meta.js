9145476091ae98095e0b9876a919a3c737942096
cosmic451 <nikhil001444@gmail.com>
2026-09-20 16:15:31 +0530

    Update author in AURELIX meta script

--- AURELIX.meta.js
+++ AURELIX.meta.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         AURELIX Auto Engine
-// @namespace    aurelix.auto.engine
+// @author       Cosmic
 // @version      0.5.57
 // @description  AURELIX Auto Engine Update Metadata
 // @updateURL    https://raw.githubusercontent.com/cosmic451/AURELIX/refs/heads/main/AURELIX.meta.js
