c10c64f723caeb9d3810e30c56f91c4055ca5a05
cosmic451 <nikhil001444@gmail.com>
2026-09-21 20:37:45 +0530

    Update metadata for AURELIX v0.5.67

--- AURELIX.meta.js
+++ AURELIX.meta.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AURELIX Auto Engine
 // @author       Cosmic
-// @version      0.5.62
+// @version      0.5.67
 // @description  AURELIX Auto Engine Update Metadata
 // @updateURL    https://raw.githubusercontent.com/cosmic451/AURELIX/refs/heads/main/AURELIX.meta.js
 // @downloadURL  https://raw.githubusercontent.com/cosmic451/AURELIX/refs/heads/main/AURELIX.user.js
